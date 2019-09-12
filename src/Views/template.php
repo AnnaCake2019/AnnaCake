26c06ef3fa1072c433df21cc6d5170eec431baed
AnnaCake2019 <55207716+AnnaCake2019@users.noreply.github.com>
2019-09-12 22:47:16 +0300

    three

--- src/Views/template.php
+++ src/Views/template.php
@@ -20,25 +20,24 @@
         </div>
         <nav class="mobile-menu" id="mobile">
             <ul>
-                <li><a onclick="hideMenu()" href="/">SOFT</a></li>
+                <li><a onclick="hideMenu()" href="/">Главная</a></li>
                 <li><a onclick="hideMenu()" href="/shop/show">Магазин</a></li>
-                <li><a onclick="hideMenu()" href="/info/contacts">Контакты</a></li>
-                <li><a onclick="hideMenu()" href="/blog/show">Ваш дизайн</a></li>
+                <li><a onclick="hideMenu()" href="/info/contacts">О нас</a></li>
+                <li><a onclick="hideMenu()" href="/blog/show">Контакты</a></li>
                 <li><a onclick="hideMenu()" href="/cart/show">Корзина</a></li>
-                <li><a onclick="hideMenu()" href="/account/show">Личный кабинет</a></li>
             </ul>
         </nav>
     </div>
 
-    <div class="logo"><a href="/"><img class="minlog" src="/img/logo.png" alt="logo"></a></div>
+    <a href="/"><img class="minlog" src="/img/logo.png" alt="logo"></a>
     <nav class="menu">
         <ul>
-            <a href="/">SOFT</a>
-            <a href="/shop/show">Магазин</a>
-            <a href="/info/contacts">Контакты</a>
-            <a href="/blog/show">Ваш дизайн</a></li>
-            <a href="/cart/show">Корзина</a></li>
-            <a href="/account/show">Личный кабинет</a>
+            <li><a href="/">Главная</a></li>
+            <li><a href="/shop/show">Магазин</a></li>
+            <li><a href="/info/contacts">О нас</a></li>
+            <!-- <li><img class="minlog1" src="/img/logo.png" alt=""></li> -->
+            <li><a href="/blog/show">Контакты</a></li>
+            <li><a href="/cart/show">Корзина</a></li>
         </ul>
     </nav>
 </div>
