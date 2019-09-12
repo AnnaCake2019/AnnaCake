ffa1acd82e32fc0954e5a8242f4530b27a40aee7
Notorious88 <voodo.2015@mail.ru>
2019-09-12 21:13:42 +0400

    two -v

---
+++ src/Views/show.php
@@ -0,0 +1,33 @@
+<div class="row">
+    <div class="offset-1 col-10 assorted">
+        <h1>Торты</h1>
+        <div class="row product">
+            <div class="col- col-3"><img src="/img/shop.jpg"><p>Название</p><p>цена</p></div>
+            <div class="col- col-3"><img src="/img/shop.jpg"><p>Название</p><p>цена</p></div>
+            <div class="col- col-3"><img src="/img/shop.jpg"><p>Название</p><p>цена</p></div>
+        </div>
+        <div class="row product">
+            <div class="col- col-3"><img src="/img/shop.jpg"><p>Название</p><p>цена</p></div>
+            <div class="col- col-3"><img src="/img/shop.jpg"><p>Название</p><p>цена</p></div>
+            <div class="col- col-3"><img src="/img/shop.jpg"><p>Название</p><p>цена</p></div>
+        </div>
+    </div>
+</div>
+<div class="row">
+    <div class="offset-1 col-10 assorted">
+        <h1>Кейки</h1>
+
+    </div>
+</div>
+<div class="row">
+    <div class="offset-1 col-10 assorted">
+        <h1>Чизкейки</h1>
+
+    </div>
+</div>
+<div class="row">
+    <div class="offset-1 col-10 assorted">
+        <h1>Выпечка</h1>
+
+    </div>
+</div>
