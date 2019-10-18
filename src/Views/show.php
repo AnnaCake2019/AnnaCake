270d4314f7ba5bf9dd516cfb297b69cf1138a096
Notorious88 <voodo.2015@mail.ru>
2019-10-18 18:07:10 +0400

    style shop

--- src/Views/show.php
+++ src/Views/show.php
@@ -1,72 +1,72 @@
 <div class="row">
     <div class="offset-1 col-10 assorted">
-        <h1>Торты</h1>
+        <h2>Торты</h2>
         <a id=“OneShow”></a>
         <div class="row offset-2 offset-xs-1 product">
-            <div class="col-xs-11 col-s-4 col-3 products">
-                <?php foreach ($cakes as $cake): ?>
+            <?php foreach ($cakes as $cake): ?>
+                <div class="col-xs-11 col-s-4 col-3 products">
                     <h2><?php echo $cake['title']; ?></h2>
                     <img src="/img/Cake/<?php echo $cake['img']; ?>">
                     <p><?php echo $cake['price']; ?></p>
                     <a class="detail" href="/Cart/ShowCake/<?php echo $cake['id']; ?>">
                         Подробнее
                     </a>
-                <?php endforeach; ?>
-            </div>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
 <div class="row">
     <div class="offset-1 col-10 assorted">
-        <h1>Кейки</h1>
+        <h2>Кейки</h2>
         <a id=“twoShow”></a>
         <div class="row offset-2 offset-xs-1 product">
-            <div class="col-xs-11 col-s-4 col-3 products">
-                <?php foreach ($pies as $pie): ?>
+            <?php foreach ($pies as $pie): ?>
+                <div class="col-xs-11 col-s-4 col-3 products">
                     <h2><?php echo $pie['title']; ?></h2>
                     <img src="/img/Pie/<?php echo $pie['img']; ?>">
                     <p><?php echo $pie['price']; ?></p>
                     <a class="detail" href="/Cart/ShowPie/<?php echo $pie['id']; ?>">
                         Подробнее
                     </a>
-                <?php endforeach; ?>
-            </div>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
 <div class="row">
     <div class="offset-1 col-10 assorted">
-        <h1>Чизкейки</h1>
+        <h2>Чизкейки</h2>
         <a id=“threeShow”></a>
         <div class="row offset-2 offset-xs-1 product">
-            <div class="col-xs-11 col-s-4 col-3 products">
-                <?php foreach ($twoChees as $twoChee): ?>
+            <?php foreach ($twoChees as $twoChee): ?>
+                <div class="col-xs-11 col-s-4 col-3 products">
                     <h2><?php echo $twoChee['title']; ?></h2>
                     <img src="/img/Cheesecake/<?php echo $twoChee['img']; ?>">
                     <p><?php echo $twoChee['price']; ?></p>
                     <a class="detail" href="/Cart/ShowChees/<?php echo $twoChee['id']; ?>">
                         Подробнее
                     </a>
-                <?php endforeach; ?>
-            </div>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
 <div class="row">
     <div class="offset-1 col-10 assorted">
-        <h1>Выпечка</h1>
+        <h2>Выпечка</h2>
         <a id=“foreShow”></a>
         <div class="row offset-2 offset-xs-1 product">
-            <div class="col-xs-11 col-s-4 col-3 products">
-                <?php foreach ($bakerys as $bakery): ?>
+            <?php foreach ($bakerys as $bakery): ?>
+                <div class="col-xs-11 col-s-4 col-3 products">
                     <h2><?php echo $bakery['title']; ?></h2>
                     <img src="/img/Bakery/<?php echo $bakery['img']; ?>">
                     <p><?php echo $bakery['price']; ?></p>
                     <a class="detail" href="/Cart/ShowBakery/<?php echo $bakery['id']; ?>">
                         Подробнее
                     </a>
-                <?php endforeach; ?>
-            </div>
+                </div>
+            <?php endforeach; ?>
         </div>
     </div>
 </div>
